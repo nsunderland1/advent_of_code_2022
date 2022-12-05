7f240a00eef1cccaa2fb3d2d61c4ec6b444e673c
nsunderland1 <sunderland.nicholas@protonmail.com>
2022-12-04 23:06:03 -0800

    Optimized?

--- src/day05.rs
+++ src/day05.rs
@@ -1,10 +1,47 @@
-use nom::{bytes::complete::tag, sequence::tuple, IResult};
+use std::{cmp::Ordering, collections::VecDeque};
+
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take},
+    combinator::{map, value},
+    multi::separated_list1,
+    sequence::{delimited, tuple},
+    IResult,
+};
 
 #[allow(unused)]
 use crate::prelude::*;
 
-fn parse_line(string: &[u8]) -> IResult<&[u8], (usize, usize, usize)> {
-    let (rem, (_, a, _, b, _, c)) = tuple((
+fn parse_stack_entry(input: &[u8]) -> IResult<&[u8], Option<u8>> {
+    alt((
+        // Empty
+        value(None, tag(b"   ")),
+        // Stack entry
+        map(
+            // Single char wrapped in square brackets
+            delimited(tag(b"["), take(1usize), tag(b"]")),
+            // Always safe to do byte[0] because we did take(1)
+            |byte: &[u8]| Some(byte[0]),
+        ),
+    ))(input)
+}
+
+fn parse_stack_row<'a>(input: &'a [u8], stacks: &mut Vec<VecDeque<u8>>) -> IResult<&'a [u8], ()> {
+    let (remaining, row) = separated_list1(tag(" "), parse_stack_entry)(input)?;
+
+    stacks.resize(row.len(), VecDeque::new());
+
+    for (i, item) in row.into_iter().enumerate() {
+        if let Some(item) = item {
+            stacks[i].push_back(item);
+        }
+    }
+
+    Ok((remaining, ()))
+}
+
+fn parse_instruction(string: &[u8]) -> IResult<&[u8], (usize, usize, usize)> {
+    let (rem, (_, count, _, from, _, to)) = tuple((
         tag("move "),
         nom::character::complete::u64,
         tag(" from "),
@@ -13,7 +50,7 @@ fn parse_line(string: &[u8]) -> IResult<&[u8], (usize, usize, usize)> {
         nom::character::complete::u64,
     ))(string)?;
 
-    Ok((rem, (a as usize, b as usize, c as usize)))
+    Ok((rem, (count as usize, from as usize - 1, to as usize - 1)))
 }
 
 pub fn run(input: &str) -> (usize, usize) {
@@ -21,49 +58,56 @@ pub fn run(input: &str) -> (usize, usize) {
 
     let mut lines = input.lines();
 
-    'outer: loop {
-        let line = lines.next().unwrap();
+    while let Ok(_) = parse_stack_row(lines.next().unwrap().as_bytes(), &mut stacks) {}
 
-        let stack_entries = line.as_bytes().chunks(4);
-        for (stack_id, stack_entry) in stack_entries.into_iter().enumerate() {
-            if stack_entry[1] == b'1' {
-                break 'outer;
+    assert_eq!(lines.next().unwrap(), "");
+
+    let instructions: Vec<_> = lines
+        .map(|line| parse_instruction(line.as_bytes()).unwrap().1)
+        .collect();
+
+    {
+        let mut stacks = stacks.clone();
+
+        for &(num, from, to) in &instructions {
+            for _ in 0..num {
+                let val = stacks[from].pop_front().unwrap();
+                stacks[to].push_front(val);
             }
+        }
 
-            let stack_char = stack_entry[1];
-            if stack_id >= stacks.len() {
-                stacks.push(Vec::new());
-            }
-
-            if stack_entry[1] == b' ' {
-                continue;
-            }
-
-            stacks[stack_id].push(stack_char as char);
+        for stack in stacks {
+            print!("{}", *stack.front().unwrap() as char);
         }
     }
 
-    assert_eq!(lines.next().unwrap(), "");
-    for stack in stacks.iter_mut() {
-        stack.reverse();
+    println!();
+
+    {
+        for (num, from, to) in instructions {
+            let (from_stack, to_stack) = match from.cmp(&to) {
+                Ordering::Less => {
+                    let (left, right) = stacks.split_at_mut(to);
+                    (&mut left[from], &mut right[0])
+                }
+                Ordering::Equal => continue,
+                Ordering::Greater => {
+                    let (left, right) = stacks.split_at_mut(from);
+                    (&mut right[0], &mut left[to])
+                }
+            };
+
+            for item in from_stack.drain(0..num).rev() {
+                to_stack.push_front(item);
+            }
+        }
+
+        for stack in stacks {
+            print!("{}", *stack.front().unwrap() as char);
+        }
     }
 
-    for line in lines {
-        let Ok((_, (num, from, to))) = parse_line(line.as_bytes()) else { break; };
-
-        // for _ in 0..num {
-        //     let val = stacks[from - 1].pop().unwrap();
-        //     stacks[to - 1].push(val);
-        // }
-
-        let split_index = stacks[from - 1].len() - num;
-        let to_move = stacks[from - 1].split_off(split_index);
-        stacks[to - 1].extend_from_slice(&to_move);
-    }
-
-    for stack in stacks {
-        print!("{}", *stack.last().unwrap());
-    }
+    println!();
 
     // println!("Part 1:");
     (0, 0)
