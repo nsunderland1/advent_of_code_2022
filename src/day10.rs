4292fb0711e7b72991495b25e8d137e4eac76f30
nsunderland1 <sunderland.nicholas@protonmail.com>
2022-12-11 15:50:13 -0800

    Clean up day 10 a bit

--- src/day10.rs
+++ src/day10.rs
@@ -7,88 +7,54 @@ enum Instruction {
 }
 
 pub fn run(input: &str) -> (Solution, Solution) {
-    let instructions = input
-        .lines()
-        .map(|line| match line {
-            "noop" => Instruction::Noop,
-            _ => {
-                let (_, value) = line.split_once(' ').unwrap();
-                Instruction::Add(value.parse().unwrap())
+    let instructions = input.lines().map(|line| match line {
+        "noop" => Instruction::Noop,
+        _ => {
+            let (_, value) = line.split_once(' ').unwrap();
+            Instruction::Add(value.parse().unwrap())
+        }
+    });
+
+    let mut total = 0;
+    let mut register = 1;
+    let mut cycle = 0;
+    let mut row = [' '; 40];
+
+    for instruction in instructions {
+        let num_cycles = match instruction {
+            Instruction::Noop => 1,
+            Instruction::Add(_) => 2,
+        };
+
+        for _ in 0..num_cycles {
+            cycle += 1;
+
+            let mod_cycle: usize = (cycle - 1) % 40;
+            if (mod_cycle as i64).abs_diff(register as i64) <= 1 && (0i32..40).contains(&register) {
+                row[mod_cycle] = '#';
             }
-        })
-        .collect_vec();
 
-    let result1 = {
-        let mut total = 0;
-        let mut register = 1;
-        let mut cycle = 0;
-
-        for instruction in instructions.iter() {
-            let num_cycles = match instruction {
-                Instruction::Noop => 1,
-                Instruction::Add(_) => 2,
-            };
-
-            for _ in 1..=num_cycles {
-                cycle += 1;
-
-                match cycle {
-                    20 | 60 | 100 | 140 | 180 | 220 => {
-                        total += cycle as i32 * register;
-                    }
-                    _ => (),
+            match cycle {
+                20 | 60 | 100 | 140 | 180 | 220 => {
+                    total += cycle as i32 * register;
                 }
-            }
-
-            match instruction {
-                Instruction::Noop => (),
-                Instruction::Add(val) => register += val,
+                40 | 80 | 120 | 160 | 200 | 240 => {
+                    let line = row.iter().copied().collect::<String>();
+                    println!("{line}");
+                    row.fill(' ');
+                }
+                _ => (),
             }
         }
 
-        total as usize
-    };
-
-    let result2 = {
-        let mut register: i32 = 1;
-        let mut cycle = 0;
-        let mut row = ['.'; 40];
-
-        for instruction in instructions.iter() {
-            let num_cycles = match instruction {
-                Instruction::Noop => 1,
-                Instruction::Add(_) => 2,
-            };
-
-            for _ in 1..=num_cycles {
-                cycle += 1;
-                // println!("{cycle} {register}");
-
-                let mod_cycle: usize = (cycle - 1) % 40;
-                if (mod_cycle as i64).abs_diff(register as i64) <= 1
-                    && (0i32..40).contains(&register)
-                {
-                    row[mod_cycle] = '#';
-                }
-
-                match cycle {
-                    40 | 80 | 120 | 160 | 200 | 240 => {
-                        let line = row.iter().copied().collect::<String>();
-                        println!("{line}");
-                        row.fill('.');
-                    }
-                    _ => (),
-                }
-            }
-
-            match instruction {
-                Instruction::Noop => (),
-                Instruction::Add(val) => register += val,
-            }
+        match instruction {
+            Instruction::Noop => (),
+            Instruction::Add(val) => register += val,
         }
+    }
 
-        0
-    };
+    let result1 = total as usize;
+    let result2 = 0;
 
     (result1.into(), result2.into())
 }
