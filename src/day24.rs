fd9482ac27a154cfc7df891854ff0d35ff1a6de0
nsunderland1 <sunderland.nicholas@protonmail.com>
2022-12-23 21:48:59 -0800

    Super easy part 2

--- src/day24.rs
+++ src/day24.rs
@@ -52,73 +52,74 @@ pub fn run(input: &str) -> (Solution, Solution) {
         .map(|line| line.chars().map(|c| Square::from(c)))
         .collect();
 
-    println!("{} {}", map.width(), map.height());
-
     let result1 = {
         let mut map = map.clone();
 
-        let mut positions = HashSet::from_iter([(1, 0)]);
-
         let mut result = 0;
 
-        println!("{map}");
+        let start = (1, 0);
+        let end = (map.width() - 2, map.height() - 1);
 
-        for num_moves in 1.. {
-            let mut next_map = grid![Square::Open(Vec::new()); map.width(), map.height()];
+        for (start, end) in [(start, end), (end, start), (start, end)] {
+            let mut positions = HashSet::from_iter([start]);
+            for num_moves in 1.. {
+                let mut next_map = grid![Square::Open(Vec::new()); map.width(), map.height()];
 
-            for y in 1..(map.height() - 1) {
-                for x in 1..(map.width() - 1) {
-                    match &map[(x, y)] {
-                        Square::Wall => (),
-                        Square::Open(blizzards) => {
-                            for blizzard in blizzards.iter().copied() {
-                                let mut next_position = (
-                                    (x as isize + blizzard.0) as usize,
-                                    (y as isize + blizzard.1) as usize,
-                                );
+                for y in 1..(map.height() - 1) {
+                    for x in 1..(map.width() - 1) {
+                        match &map[(x, y)] {
+                            Square::Wall => (),
+                            Square::Open(blizzards) => {
+                                for blizzard in blizzards.iter().copied() {
+                                    let mut next_position = (
+                                        (x as isize + blizzard.0) as usize,
+                                        (y as isize + blizzard.1) as usize,
+                                    );
 
-                                if next_position.0 == 0 {
-                                    next_position.0 = map.width() - 2;
-                                } else if next_position.0 == map.width() - 1 {
-                                    next_position.0 = 1;
-                                } else if next_position.1 == 0 {
-                                    next_position.1 = map.height() - 2;
-                                } else if next_position.1 == map.height() - 1 {
-                                    next_position.1 = 1;
-                                }
+                                    if next_position.0 == 0 {
+                                        next_position.0 = map.width() - 2;
+                                    } else if next_position.0 == map.width() - 1 {
+                                        next_position.0 = 1;
+                                    } else if next_position.1 == 0 {
+                                        next_position.1 = map.height() - 2;
+                                    } else if next_position.1 == map.height() - 1 {
+                                        next_position.1 = 1;
+                                    }
 
-                                let Square::Open(next_blizzards) = &mut next_map[next_position] else {
+                                    let Square::Open(next_blizzards) = &mut next_map[next_position] else {
                                     unreachable!();
                                 };
-                                next_blizzards.push(blizzard);
+                                    next_blizzards.push(blizzard);
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            let mut next_positions = HashSet::default();
+                let mut next_positions = HashSet::default();
 
-            for position in positions.into_iter() {
-                for option in map.neighbours_orthogonal(position).chain([position]) {
-                    if map[option] != Square::Wall && next_map[option] == Square::Open(Vec::new()) {
-                        next_positions.insert(option);
+                for position in positions.into_iter() {
+                    for option in map.neighbours_orthogonal(position).chain([position]) {
+                        if map[option] != Square::Wall
+                            && next_map[option] == Square::Open(Vec::new())
+                        {
+                            next_positions.insert(option);
+                        }
                     }
                 }
-            }
 
-            positions = next_positions;
+                positions = next_positions;
 
-            for y in 1..(map.height() - 1) {
-                for x in 1..(map.width() - 1) {
-                    map[(x, y)] = next_map[(x, y)].clone();
+                for y in 1..(map.height() - 1) {
+                    for x in 1..(map.width() - 1) {
+                        map[(x, y)] = next_map[(x, y)].clone();
+                    }
                 }
-            }
 
-            let end: (usize, usize) = (map.width() - 2, map.height() - 1);
-            if positions.contains(&end) {
-                result += num_moves;
-                break;
+                if positions.contains(&end) {
+                    result += num_moves;
+                    break;
+                }
             }
         }
 
